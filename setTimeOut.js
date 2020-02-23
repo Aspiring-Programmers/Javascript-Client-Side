205ae9c3d8323df386eaf8cf416c0b5b794481d5
Nilay Datta <nilay.datta@live.com>
2020-02-23 23:10:40 +0530

    Adding files

---
+++ setTimeOut.js
@@ -0,0 +1,2 @@
+// Delay - 1 second timeout
+setTimeout(function() {}, 1000);
---
+++ using-scripts.js
@@ -0,0 +1,6 @@
+// On page script
+<script type="text/javascript">  ...
+</script>
+
+// Include external JS file
+<script src="filename.js"></script>
